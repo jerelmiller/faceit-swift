359d1addc3acad4756c3eeb51315764c4bb0bde9
Jerel Miller <jerelmiller@gmail.com>
2016-07-12 01:06:04 -0600

    Create emotions and update prepareForSegue to set proper expression

--- FaceIt/EmotionsViewController.swift
+++ FaceIt/EmotionsViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class EmotionsViewController: UIViewController {
+  private let emotionalFaces: [String: FacialExpression] = [
+    "angry": FacialExpression(eyes: .Closed, eyeBrows: .Furrowed, mouth: .Frown),
+    "happy": FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile),
+    "worried": FacialExpression(eyes: .Open, eyeBrows: .Relaxed, mouth: .Smirk),
+    "mischievious": FacialExpression(eyes: .Open, eyeBrows: .Furrowed, mouth: .Grin)
+  ]
+  
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if let faceVC = segue.destinationViewController as? FaceViewController {
+      if let identifier = segue.identifier {
+        if let expression = emotionalFaces[identifier] {
+          faceVC.expression = expression
+        }
+      }
+    }
   }
 }
