02e1e13503d399c1a7f0da5127dc66a762680913
Jerel Miller <jerelmiller@gmail.com>
2016-07-12 00:54:20 -0600

    Add split view controller. Create EmotionsViewController. Connect split view controller to appropriate view controllers

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Jerel Miller on 7/12/16.
+//  Copyright © 2016 Jerel Miller. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  }
+}
