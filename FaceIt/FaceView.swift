bdadf823a1cce990ae001eb7efa7f21b74edf693
Jerel Miller <jerelmiller@gmail.com>
2016-07-11 22:47:44 -0600

    Draw both eyes on the face

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -17,6 +17,11 @@ class FaceView: UIView {
     static let SkullRadiusToMouthOffset: CGFloat = 3
   }
   
+  private enum Eye {
+    case Left
+    case Right
+  }
+  
   var scale: CGFloat = 0.90
   
   private var skullRadius: CGFloat {
@@ -28,16 +33,43 @@ class FaceView: UIView {
   }
   
   override func drawRect(rect: CGRect) {
-    let skull = UIBezierPath(
-      arcCenter: skullCenter,
-      radius: skullRadius,
+    UIColor.blueColor().set()
+    pathForCircleCenteredAtPoint(skullCenter, withRadius: skullRadius).stroke()
+    pathForEye(.Left).stroke()
+    pathForEye(.Right).stroke()
+  }
+  
+  private func pathForEye(eye: Eye) -> UIBezierPath {
+    let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
+    let eyeCenter = getEyeCenter(eye)
+    
+    return pathForCircleCenteredAtPoint(eyeCenter, withRadius: eyeRadius)
+  }
+  
+  private func getEyeCenter(eye: Eye) -> CGPoint {
+    let eyeOffset = skullRadius / Ratios.SkullRadiusToEyeOffset
+    var eyeCenter = skullCenter
+    eyeCenter.y -= eyeOffset
+    
+    switch eye {
+    case .Left: eyeCenter.x -= eyeOffset
+    case .Right: eyeCenter.x += eyeOffset
+    }
+    
+    return eyeCenter
+  }
+  
+  private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
+    let path = UIBezierPath(
+      arcCenter: midPoint,
+      radius: radius,
       startAngle: 0.0,
       endAngle: CGFloat(2 * M_PI),
       clockwise: true
     )
     
-    skull.lineWidth = 5.0
-    UIColor.blueColor().set()
-    skull.stroke()
+    path.lineWidth = 5.0
+    
+    return path
   }
 }
