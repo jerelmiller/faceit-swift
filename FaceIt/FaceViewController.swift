d22f5b244561bbb0390b2f5674b313b5b83b1c5c
Jerel Miller <jerelmiller@gmail.com>
2016-07-11 23:47:13 -0600

    Hook up faceView to controller. Add implementation for updating UI when model changes.

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -12,8 +12,43 @@ class FaceViewController: UIViewController {
   var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile) {
     didSet { updateUI() }
   }
+  @IBOutlet weak var faceView: FaceView! {
+    didSet { updateUI() }
+  }
+  
+  private var mouthCurvatures = [
+    FacialExpression.Mouth.Frown: -1.0,
+    .Grin: 0.5,
+    .Smile: 1.0,
+    .Smirk: -0.5,
+    .Neutral: 0.0
+  ]
+  
+  private var eyeBrowTilts = [
+    FacialExpression.EyeBrows.Relaxed: 0.5,
+    .Furrowed: -0.5,
+    .Normal: 0.0
+  ]
   
   private func updateUI() {
-    
+    drawEyes()
+    drawMouth()
+    drawEyeBrows()
+  }
+  
+  private func drawEyes() {
+    switch expression.eyes {
+    case .Open: faceView.eyesOpen = true
+    case .Closed: faceView.eyesOpen = false
+    case .Squinting: faceView.eyesOpen = false
+    }
+  }
+  
+  private func drawMouth() {
+    faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+  }
+  
+  private func drawEyeBrows() {
+    faceView.eyebrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
   }
 }
