24246e7ae606e1f806bdbdea9c6087443d12e8bf
Jerel Miller <jerelmiller@gmail.com>
2016-07-11 22:03:59 -0600

    Rename ViewController to FaceViewController

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Jerel Miller on 7/11/16.
+//  Copyright © 2016 Jerel Miller. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+  
+}
